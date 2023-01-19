b00decf9dd229f4bcd38c9072b8dfbac0d0a6cab
Pradyumna <Pradyumna@TYSS-Pradyumna>
2023-01-19 17:24:28 +0530

    First Commit

---
+++ Automation_CI_Maven/src/main/java/test/Automation_CI_Maven/App.java
@@ -0,0 +1,13 @@
+package test.Automation_CI_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Automation_CI_Maven/src/test/java/test/Automation_CI_Maven/SampleTest.java
@@ -0,0 +1,28 @@
+package test.Automation_CI_Maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class SampleTest {
+	
+	
+	@Test
+	public void Demo() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");		
+		//Launch the chrome browser
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);	
+		driver.findElement(By.id("small-searchterms")).sendKeys("Books");
+		driver.findElement(By.xpath("(//input[@type='submit'])[1]")).click();
+		driver.close();
+	}
+
+}
